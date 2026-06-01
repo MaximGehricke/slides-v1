1d5a64612adf7965775ec164b92071f3c1f3b485
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2026-06-01 21:25:46 +0100

    fixed wrong vid name

--- slides/10.tsx
+++ slides/10.tsx
@@ -1,4 +1,4 @@
 import VideoSlide from "@/components/VideoSlide";
 export default function Slide12() {
-  return <VideoSlide src="https://pub-b7c5f41c03ef46718a1713db88239f83.r2.dev/ASSTS.mp4" />;
+  return <VideoSlide src="https://pub-b7c5f41c03ef46718a1713db88239f83.r2.dev/ASSTS_web.mp4" />;
 }
